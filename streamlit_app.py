51988901e8c05103b9fa64b2ca2b57d0e728d4a5
ArthurVillela <arthur.villela1@gmail.com>
2024-09-04 22:08:42 -0300

    new commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,9 +16,14 @@ with st.sidebar:
     github_url ="https://github.com/ArthurVillela1"
     st.markdown(f'<a href="{linkedin_url}" target="_blank" style="text-decoration: none; color: inherit;"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="25" height="25" style="vertical-align: middle; margin-right: 10px;"><a href="{github_url}" target="_blank" style="text-decoration: none; color: inherit;"><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" width="25" height="25" style="vertical-align: middle; margin-right: 10px;"></a>', unsafe_allow_html=True)
     st.sidebar.write("--------------------------")
+    tickers = st.text_input('Stock Tickers', 'META GOOGL')
+    start_date = st.date_input('Start Date', value=pd.to_datetime('2022-01-01'))
+    end_date = st.date_input('End Date', value=pd.to_datetime('today'))
     portfolio_val = st.number_input('Portfolio Value (USD)', value=100000)
     confidence_lv = st.slider('Confidence level', min_value=0.90, max_value=0.99, value=0.95, step=0.01)
     calculate_btn = st.button('Calculate VaR')
+
+tickers_list = tickers.split(" ")
 
 var_method = st.selectbox("Select VaR Method", ["Historical", "Parametric", "Monte Carlo Simulations"])
 
@@ -33,7 +38,6 @@ def var_calculation(returns, confidence_level, method, portfolio_value):
     elif method == "Monte Carlo":
         var = ""
     return var
-
 
 st.subheader(f"Value at Risk for your portfolio at {int(confidence_lv*100)}% confidence level:")
 #st.title(f":blue-background[{var_calculation(stock_returns, confidence_lv, var_method, portfolio_val)}]")
